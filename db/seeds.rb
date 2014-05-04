d1a18baf56d5221964fa4c8359e6b9cd3884c189
Greg Molnar <molnargerg@gmail.com>
2014-05-04 21:54:15 +0100

    scaffolding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+range = [*1...100]
+100.times do
+  Product.create(name: Faker::Commerce.product_name, price: range.sample)
+end
