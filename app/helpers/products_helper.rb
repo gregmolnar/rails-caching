9327151df7a7117dbb453d46a151523ff6f34633
Greg Molnar <molnargerg@gmail.com>
2014-05-04 22:38:26 +0100

    generate cache_key

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def cache_key_for_products(products, suffix = '')
+    ids = products.pluck(:id).join('-')
+    max_updated_at = products.pluck(:updated_at).max
+    "products/#{ids}-#{max_updated_at.to_i}#{suffix}"
+  end
 end
